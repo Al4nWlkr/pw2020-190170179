8f8aeb66dc3e54434b2958e984546c7082e439e4
bryankazuro04 <mhdalana211201@gmail.com>
2021-09-04 19:34:37 +0700

    memperbaiki typo pada index.php

--- Kuliah/Pembelajaran 5/index.php
+++ Kuliah/Pembelajaran 5/index.php
@@ -99,7 +99,7 @@ if (isset($_POST['cari'])) {
       <a href="https://youtube.com" target="_blank" class="sosmed"><i class="fab fa-youtube"></i></a>
       <a href="https://instagram.com" target="_blank" class="sosmed"><i class="fab fa-instagram"></i></a>
       <a href="https://twitter.com" target="_blank" class="sosmed"><i class="fab fa-twitter"></i></a>
-      <a href="https://facebook.com" target="_blank" rel="nofollow" class="sosmed"><i class="fab fa-facebook-f"></i></a>
+      <a href="https://facebook.com" target="_blank" class="sosmed"><i class="fab fa-facebook-f"></i></a>
     </div>
     <p>&copy; Developed By Bryan Kazuro</p>
   </footer>
